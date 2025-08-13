97b98f7d3333957c92e4a9cd6b7f3e6c655f79a5
ams-mikhail <alphaglasson@mail.ru>
2025-08-13 22:35:45 +0700

    add migration for task

---
+++ database/migrations/2025_08_12_121649_create_tasks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->dateTime('due_date');
+            $table->enum('status', ['new', 'in_progress', 'done'])->default('new');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
