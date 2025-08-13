2e2073fd0c16b0f0e5469f1b3775160111dbc939
ams-mikhail <alphaglasson@mail.ru>
2025-08-13 22:13:43 +0700

    add migration: update user fields

---
+++ database/migrations/2025_08_12_111611_modify_users_table_for_login_auth.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['name', 'email', 'email_verified_at']);
+            $table->string('username')->unique()->after('id');
+        });
+
+        Schema::dropIfExists('password_reset_tokens');
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name')->after('id');
+            $table->string('email')->unique()->after('name');
+            $table->timestamp('email_verified_at')->nullable()->after('email');
+            $table->dropColumn('login');
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+};
