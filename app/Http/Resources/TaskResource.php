a92543f96609d89303ff68b87b737586d0625132
ams-mikhail <alphaglasson@mail.ru>
2025-08-15 16:49:43 +0700

    Create TaskResource.php

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title ?? '',
+            'description' => $this->description ?? '',
+            'due_date' => $this->due_date?->format('Y-m-d') ?? '',
+            'status' => $this->status ?? '',
+            'created_at' => $this->created_at?->format('Y-m-d H:i') ?? '',
+//            'updated_at' => $this->updated_at->format('Y-m-d H:i'),
+        ];
+    }
+}
