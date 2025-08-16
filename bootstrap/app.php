2db03492fb397f39dd1dd030dd8e699a1c1738d2
ams-mikhail <alphaglasson@mail.ru>
2025-08-16 16:10:20 +0700

    global catching ValidationException

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -37,6 +38,14 @@ return Application::configure(basePath: dirname(__DIR__))
                 'status'  => 'error',
                 'message' => 'Требуется авторизация.'
             ], 403);
+        });
+
+        $exceptions->render(function (ValidationException $e) {
+            return response()->json([
+                'status'  => 'error',
+                'message' => 'Ошибка валидации.',
+                'errors'  => $e->errors(),
+            ], 422);
         });
 
     })->create();
